0134dec4c1d14352f5811bf95c11817d33fe6ab9
Martín González Buitrón <martingonzalezbuitron@gmail.com>
2023-06-13 10:57:33 -0300

    Agregada IOrganization.java

---
+++ src/model/IOrganization.java
@@ -0,0 +1,11 @@
+package model;
+
+import model.ZoneCoverage;
+import model.Sample;
+
+import java.util.List;
+
+public interface IOrganization {
+
+	public void Update(ZoneCoverage aZone, Sample aSample);
+}
