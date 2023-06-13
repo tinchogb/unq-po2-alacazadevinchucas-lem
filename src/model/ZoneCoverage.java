d0e07d4a9088cdea95d0be1547e4f378128c5943
Martín González Buitrón <martingonzalezbuitron@gmail.com>
2023-06-13 10:55:33 -0300

    Agregada interfaz IZoneCoverage a ZoneCoverage

--- src/model/ZoneCoverage.java
+++ src/model/ZoneCoverage.java
@@ -1,13 +1,16 @@
 package model;
 
+import model.Sample;
 import java.util.List;
 
-public class ZoneCoverage {
+public class ZoneCoverage implements IZoneCoverage{
 
 	private System 		system = System.getInstance("lalala");
 	private String 	 	name;
 	private Location 	epicenter;
 	private Integer  	radiusInKm;
+
+	public List<IOrganization> organizations;
 
 	// Constructor for testing purpose only 
 	public ZoneCoverage(System aSystem, String name, Location epicenter, Integer radiusInKm) {
@@ -25,12 +28,28 @@ public class ZoneCoverage {
 		this.radiusInKm	= radiusInKm;
 	}
 
+	@Override
+	public void Attach(Organization anOrg) {};
+	@Override
+	public void Detach(Organization anOrg) {};
+	@Override
+	public void Notify(Sample aSample) {
+		this.organizations.stream().forEach(org -> org.Update(this, aSample));
+	}
+
+	// Quizás no se necesitan ya que se pasa el cambio por parámetro en el Notify.
+	public void GetState() {};
+	public void SetState() {};
+	public void subjectState() {};
+
+	public void Update(Sample aSample) { // ZoneCoverage es intermediario en el cambio de estado.
+		this.Notify(aSample);
+	}
 	
 	public System 	getSystem() 		{ return system; }
 	public String 	getName() 			{ return name; }
 	public Location getEpicenter() 		{ return epicenter; }
 	public Integer 	getRadiusInKm() 	{ return radiusInKm; }
-
 
 	public List<Sample> samplesInZone() {
 		// TODO Auto-generated method stub
