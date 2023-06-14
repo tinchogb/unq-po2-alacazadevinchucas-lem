b864bd9cc1b6842b8d5168a87c062693c2d12773
Martín González Buitrón <martingonzalezbuitron@gmail.com>
2023-06-13 21:42:21 -0300

    Cambios menores.

--- src/model/Location.java
+++ src/model/Location.java
@@ -1,16 +1,14 @@
 package model;
 
-import java.lang.Math.*;
 import java.util.List;
 
 public class Location {
 
-	private System 			system = System.getInstance("lalala");
+	private System 			system = System.getInstance("systemLEM");
 	private float 			latitude;
 	private float 			longitude;
 
 	public Location(float aLatitude, float aLongitude) {
-		// TODO Auto-generated constructor stub
 		this.latitude 	= aLatitude;
 		this.longitude	= aLongitude;
 	}
@@ -19,7 +17,6 @@ public class Location {
 	public float getLongitude() { return longitude; }
 
 	public double distance(Location aLocation1, Location aLocation2) {
-		// TODO Auto-generated method stub
 		// Taken from: https://stackoverflow.com/questions/14431032/i-want-to-calculate-the-distance-between-two-points-in-java
 		return Math.hypot( (aLocation1.getLatitude()  - aLocation2.getLatitude())
 						 , (aLocation1.getLongitude() - aLocation2.getLongitude()) );
@@ -29,19 +26,16 @@ public class Location {
 	}
 
 	public double distance(Location aLocation) {
-		// TODO Auto-generated method stub
 		return this.distance(this, aLocation);
 	}
 
 	public List<Location> closestLocations(List<Location> locations, double maxDistance) {
-		// TODO Auto-generated method stub
 		return locations.stream()
 				.filter(location -> this.distance(location) <= maxDistance)
 				.toList();
 	}
 
 	public List<Sample> closestSamples(Sample aSample, double maxDistance) {
-		// TODO Auto-generated method stub
 		return this.system.getSamples().stream()
 				.filter(sample -> this.distance(aSample.getLocation(), sample.getLocation()) <= maxDistance)
 				.toList();
