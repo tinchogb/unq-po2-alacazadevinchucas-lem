eb59a5284fbf6f3121d1ef4f7c358c907ab8dda2
Martín González Buitrón <martingonzalezbuitron@gmail.com>
2023-06-13 21:40:00 -0300

    Agregados tests a LocationTest.java

--- test/model/LocationTest.java
+++ test/model/LocationTest.java
@@ -8,40 +8,95 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+import java.util.ArrayList;
 
-import model.ZoneCoverage;
-import model.Organization;
 
 public class LocationTest {
 
-	public float latitude;
-	public float longitude;
+	private Location 		location;
 
-	public Location 				location;
-	public Location 				location1;
-	public Location 				location2;
-	public List<Location>			givenLocations;
-	public Sample 					sample;
-	public Integer					distance1;
-	public ZoneCoverage				zone1;
+	private System 	system = System.getInstance("systemLEM");
+	private float 	latitude;
+	private float 	longitude;
+
+
+	private Location 				location1;
+	private Location 				location2;
+	private Sample 					sample1;
+	private Sample 					sample2;
+	private Sample 					sample3;
+	private Integer					distance1;
 
 	@BeforeEach
 	public void setUp() {
 	// DOC (Depended-On-Component): nuestros doubles
-	
+		this.latitude 	= 5;
+		this.longitude	= 5;
+		
+		this.location1 	= mock(Location.class);
+		this.location2	= mock(Location.class);
+		when(this.location1.getLatitude()).thenReturn((float) 4);
+		when(this.location1.getLongitude()).thenReturn((float) 4);
+		when(this.location2.getLatitude()).thenReturn((float) 2);
+		when(this.location2.getLongitude()).thenReturn((float) 2);
+		this.sample1 	= mock(Sample.class);
+		this.sample2 	= mock(Sample.class);
+		this.sample3 	= mock(Sample.class);
+		this.distance1	= 2;
+
 	// SUT (System Under Test): objeto a testear
 	location = new Location(latitude, longitude);
 	}
-	
+
 	@Test
-	public void unTest() {
-	// Exercise
+	void testConstructor() {
+		assertFalse(this.location==null);
+	}
+
+	@Test
+	public void testGetLatitude() {
+		assertEquals(this.latitude, this.location.getLatitude());
+	}
+
+	@Test
+	public void testGetLongitude() {
+		assertEquals(this.longitude, this.location.getLongitude());
+	}
+
+	@Test
+	public void testDistanceTwoLocations() {
+		assertEquals(2, this.location.distance(location1, location2));
 	}
 	
 	@Test
-	public void otroTest() {
-		double distance					= location.distance(location1, location2);
-		List<Location> knownLocations 	= location.closestLocations(givenLocations);
-		List<Sample> knownSamples 		= location.closestSamples(sample, distance1);
+	public void testDistanceOneLocation() {
+		when(this.location1.getLatitude()).thenReturn((float) 3);
+		when(this.location1.getLongitude()).thenReturn((float) 3);
+		assertEquals(2, this.location.distance(location1));
+	}
+
+	@Test
+	public void testClosestSamples() {
+		when(this.sample1.getLocation()).thenReturn(location);
+		when(this.sample2.getLocation()).thenReturn(location1);
+		when(this.sample3.getLocation()).thenReturn(location2);
+		this.system = spy(this.system);
+		List<Sample> givenSamples = new ArrayList<>();
+		givenSamples.add(sample2);
+		givenSamples.add(sample3);
+		when(this.system.getSamples()).thenReturn(givenSamples);
+		assertEquals(givenSamples, this.location.closestSamples(sample1, this.distance1));
+	}
+
+	@Test
+	public void testClosestLocations() {
+		Location location3 = mock(Location.class);
+		when(location3.getLatitude()).thenReturn((float) 7);
+		when(location3.getLongitude()).thenReturn((float) 7);
+		List<Location> givenLocations = new ArrayList<>();
+		givenLocations.add(this.location1);
+		givenLocations.add(this.location2);
+		givenLocations.add(location3);
+		assertEquals(givenLocations.remove(location3), this.location.closestLocations(givenLocations, distance1));
 	}
 }
