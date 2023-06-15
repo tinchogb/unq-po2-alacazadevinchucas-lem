bcd55ac102c5fef827041eb00ba18149f64f4bbb
Martín González Buitrón <martingonzalezbuitron@gmail.com>
2023-06-15 00:17:51 -0300

    Agregados tests para terminar

--- test/model/SystemTest.java
+++ test/model/SystemTest.java
@@ -8,16 +8,25 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
 
 public class SystemTest {
 
-	public static System system;
+	public System system;
 
 	public static System instance;
-	private List<ZoneCoverage> listenerZones;
-	public  String				name;
+	private List<ZoneCoverage> listenerZones = new ArrayList<ZoneCoverage>();
 
+	private User user1;
+	private User user2;
+	private Sample sample1;
+	private Sample sample2;
+	private Location location1;
+	private Location location2;
+	private ZoneCoverage zone1;
+	private ZoneCoverage zone2;
+	
 	private  List<User>			users     = new ArrayList<User>(); 
 	private  List<Sample>		samples   = new ArrayList<Sample>(); 
 	private  List<Location>		locations = new ArrayList<Location>();
@@ -27,12 +36,17 @@ public class SystemTest {
 	@BeforeEach
 	public void setUp() {
 		// DOC (Depended-On-Component): nuestros doubles
-		this.instance		 = mock(System.class);
-		when(this.instance.getInstance()).thenCallRealMethod();
+//		this.instance = mock(System.class);
+//		when(this.instance.getInstance()).thenReturn(this.instance);
+//		try (MockedStatic<System> instance = mockStatic(System.class)) {
+//			instance.when(System::getInstance).thenReturn(this);
+//			assertEquals(instance, System.getInstance());
+//			instance.verify(System::getInstance);
+//		}
 
 
 //		this.organization = mock(Organization.class);
-		this.listenerZones = mock(List<ZoneCoverage.class>);
+		this.listenerZones = (List<ZoneCoverage>) mock(List.class);
 		this.user1 	   	= mock(User.class);
 		this.user2 	   	= mock(User.class);
 		this.sample1 	= mock(Sample.class);
@@ -55,13 +69,75 @@ public class SystemTest {
 	}
 	
 	@Test
-	void testGetInstancee() {
-		assertEquals("foo", Foo.method());
-		try (MockedStatic mocked = mockStatic(Foo.class)) {
-			mocked.when(Foo::method).thenReturn("bar");
-			assertEquals("bar", Foo.method());
-			mocked.verify(Foo::method);
+	void testGetInstance() {
+		try (MockedStatic<System> mocked = mockStatic(System.class)) {
+			mocked.when(System::getInstance).thenReturn(this);
+			assertEquals(mocked, System.getInstance());
+			mocked.verify(System::getInstance);
 		}
-		assertEquals("foo", Foo.method());
+	}
+
+	@Test
+	void testGetUsers() {
+	}
+	
+	@Test
+	void testGetSamples() {
+	}
+	
+	@Test
+	void testGetLocations() {
+	}
+	
+	@Test
+	void testGetZones() {
+	}
+
+	@Test
+	void testAddForUsers() {
+	}
+
+	@Test
+	void testAddForSamples() {
+	}
+
+	@Test
+	void testAddForLocations() {
+	}
+
+	@Test
+	void testAddForZones() {
+	}
+
+	@Test
+	void testIsInForUsers() {
+	}
+
+	@Test
+	void testIsInForSamples() {
+	}
+
+	@Test
+	void testIsInForLocations() {
+	}
+
+	@Test
+	void testIsInForZones() {
+	}
+	
+	@Test
+	void testIsInListeners() {
+	}
+
+	@Test
+	void testAttach() {
+	}
+
+	@Test
+	void testDetach() {
+	}
+
+	@Test
+	void testNotify() {
 	}
 }
